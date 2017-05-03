a62993e80229de64633fdd56a16608d82b42dae5
alisafrunza <alisafrunza@gmail.com>
2017-05-03 19:07:26 +0300

    first commit

---
+++ app/controllers/cases_controller.rb
@@ -0,0 +1,44 @@
+class CasesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+  end
+
+  def show
+    @case = Case.find(params[:id])
+  end
+
+  def new
+    @case = Case.new
+  end
+
+  def create
+    @case = Case.create(case_params)
+    @case.user = current_user
+    @case.save
+    redirect_to @case
+  end
+
+  def edit
+    @case = Case.find(params[:id])
+  end
+
+  def update
+    @case = Case.find(params[:id])
+    if @case.update_attributes(case_params)
+      redirect_to @case
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @case = Case.find(params[:id])
+    @case.destroy
+    redirect_to cases_path
+  end
+
+  def case_params
+    params.require(:case).permit!
+  end
+end
